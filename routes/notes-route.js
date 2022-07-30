f64921fe8d31bc4d431d8a4bc956bc61ce4608eb
DanielK <Danielkang13@gmail.com>
2022-07-30 15:06:37 -0700

    added get request for getting the new html page and the db

--- routes/notes-route.js
+++ routes/notes-route.js
@@ -1,4 +1,10 @@
 const express = require('express')
+const fs = require('fs');
+const db = require("../db/db.json");
 const router = express.Router();
+
+router.get("/notes", (req, res) => {
+    res.send(db);
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const fs = require('fs');
-const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3002;
+
+const app = express();
 
 const notes = require("./routes/notes-route");
 app.use('/api', notes);
 
 app.use(express.static('public')); //looks for the index.html file or the landing page in the public folder
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
 
 app.listen(PORT, () => {
     console.log('Listening on PORT 3002');
