ab973e9458238177590c3c5b585f900feb6d946a
DanielK <Danielkang13@gmail.com>
2022-07-30 16:30:32 -0700

    added DELETE request

--- routes/notes-route.js
+++ routes/notes-route.js
@@ -15,4 +15,14 @@ router.post("/notes", (req, res) => { //req will be an stringified object with "
     res.send(db);
 })
 
+router.delete("/notes/:id", (req, res) => {
+    const targetId = req.params.id;
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].id === targetId) {
+            db.splice(i,1);
+        }
+    }
+    res.end();
+})
+
 module.exports = router;
